43ae83a8e6104894272b078f9b6c223347459b19
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-11-04 23:12:59 -0500

    Copying css dependencies

--- lib/fangorn/haml.rb
+++ lib/fangorn/haml.rb
@@ -21,6 +21,18 @@ module Fangorn
     end
 
     def css_include(file)
+      copy_file file do |src, dest, css|
+        css.scan(/url\(["']([\w\-\.\/]+).*?["']\)/).flatten.uniq.each do |path|
+          reference = File.join(src, path)
+          if File.exists? reference
+            out = File.join(dest, path)
+            FileUtils.mkdir_p File.dirname(out)
+            File.open(out, 'w') do |f|
+              f.write File.read(reference)
+            end
+          end
+        end
+      end
       ::Haml::Engine.new("%link{ rel: 'stylesheet', type: 'text/css', href: '#{cache_update file}' }").render
     end
 
@@ -31,23 +43,30 @@ module Fangorn
     end
 
     def js_include(file)
-      SCRIPT_SOURCES.each do |refdir, sourcedir|
-        src = file.sub /^#{refdir}\//, "#{sourcedir}/"
-        if File.exists? src
-          out = File.join(Output::dest, file)
-          FileUtils.mkdir_p File.dirname(out)
-          File.open(out, 'w') do |f|
-            f.write File.read(src)
-          end
-          break
-        end
-      end
+      copy_file file
       ::Haml::Engine.new("%script{ src: '#{cache_update file}' }").render
     end
     def js_include_tree(dir)
       Dir[File.join(Output::dest, dir, '**', '*.js')].sort_by(&:length).map do |file|
         js_include file.sub File.join(Output::dest, ''), ''
       end.join
+    end
+
+    def copy_file(file)
+      SCRIPT_SOURCES.each do |refdir, sourcedir|
+        src = file.sub /^#{refdir}\//, "#{sourcedir}/"
+        if File.exists? src
+          out = File.join(Output::dest, file)
+          FileUtils.mkdir_p File.dirname(out)
+          File.open(out, 'w') do |f|
+            contents = File.read(src)
+            f.write contents
+
+            yield File.dirname(src), File.dirname(out), contents if block_given?
+          end
+          break
+        end
+      end
     end
 
     def cache_update(file)
